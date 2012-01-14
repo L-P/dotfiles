f1fcde840e05b7fd3f3c6b73abb393fc34f9882e
Léo Peltier <contact@leo-peltier.fr>
2012-01-14 22:20:19 +0100

    Removed useless check.
    No need to check brightness boundaries, the kernel does it well enough.

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -5,18 +5,10 @@ if [[ -z "$1" || -z "$2" ]]; then
 	exit 1
 fi
 
-dir='/sys/class/backlight/intel_backlight/'
-file_cur="$dir/brightness"
-file_max="$dir/max_brightness"
-
-cur=$(cat "$file_cur")
-max=$(cat "$file_max")
+file='/sys/class/backlight/intel_backlight/brightness'
+cur=$(cat "$file")
 
 new=$(($cur $1 $2))
-if [[ $new -gt $max ]]; then
-	new=$max
-fi
-
-echo $new > $file_cur
+echo $new > $file
 echo New brightness: $new/$max
 
