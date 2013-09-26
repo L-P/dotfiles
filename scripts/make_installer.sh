417cd746b45917c0c5a921a6082a19c09237f549
Léo Peltier <contact@leo-peltier.fr>
2013-09-26 15:15:57 +0200

    Fix submodules .git included in dotfiles.run.

--- scripts/make_installer.sh
+++ scripts/make_installer.sh
@@ -8,7 +8,7 @@ function main() {
     git clone "$repo" "$tmp"
     pushd "$tmp"
     git submodule update --init --recursive
-    rm -Rf .git
+    find . -type d -name '.git' -exec rm -rf {} +
     pushd ..
     makeself "$tmp" "$pwd/$(basename "$tmp").run" dotfiles ./loader
     popd; popd
